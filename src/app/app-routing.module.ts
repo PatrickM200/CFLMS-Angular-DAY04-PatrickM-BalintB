b775d9e89955df243d5089a0cd38f3a6eb96680f
Patrick M <miglar.patrick@gmail.com>
2020-06-25 10:53:02 +0200

    blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'blog', component: BlogPageComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: '**', redirectTo: '/blog'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPageComponent,
+    AboutPageComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
